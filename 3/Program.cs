ae485b7307bf2ee1038bd72d2077b9dcddbd259c
Kerem Göksu <superkerem13@gmail.com>
2024-12-03 22:06:28 +0300

    Day 3

---
+++ 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+int sum = 0;
+
+string input = File.ReadAllText("input");
+string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+
+foreach (Match m in Regex.Matches(input, pattern, RegexOptions.Multiline))
+{
+    int a = Convert.ToInt32(m.Groups[1].Value);
+    int b = Convert.ToInt32(m.Groups[2].Value);
+    sum += a * b;
+}
+
+Console.WriteLine(sum);
---
+++ 3_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+int sum = 0;
+bool mulEnabled = true;
+
+string input = File.ReadAllText("input");
+string pattern = @"mul\((\d{1,3}),(\d{1,3})\)|do(n\'t)?\(\)";
+
+foreach (Match m in Regex.Matches(input, pattern, RegexOptions.Multiline))
+{
+    if (m.Value.StartsWith("mul"))
+    {
+        if (mulEnabled)
+        {
+            int a = Convert.ToInt32(m.Groups[1].Value);
+            int b = Convert.ToInt32(m.Groups[2].Value);
+            sum += a * b;
+        }
+    }
+    else if (m.Value.StartsWith("do"))
+    {
+        mulEnabled = !m.Value.StartsWith("don't");
+    }
+}
+
+Console.WriteLine(sum);
