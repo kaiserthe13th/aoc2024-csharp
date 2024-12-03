14a8576587c5a5457775055aec26bb889915a530
Kerem Göksu <superkerem13@gmail.com>
2024-12-03 22:06:09 +0300

    Day 1

---
+++ 1/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] lines = File.ReadAllLines("input");
+Span<int> list1 = stackalloc int[lines.Length];
+int list1Length = 0;
+Span<int> list2 = stackalloc int[lines.Length];
+int list2Length = 0;
+
+foreach (var line in lines) {
+    string[] strings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int i = Convert.ToInt32(strings[0]);
+    int j = Convert.ToInt32(strings[1]);
+    list1[list1Length++] = i;
+    list2[list2Length++] = j;
+}
+
+list1.Sort();
+list2.Sort();
+
+int differenceSum = 0;
+
+for (int i = 0; i < list1Length; i++) {
+    int a = list1[i];
+    int b = list2[i];
+    int difference = Math.Abs(a - b);
+    Console.WriteLine($"{a}\t{b}\t\t{difference}");
+    differenceSum += difference;
+}
+
+Console.WriteLine(differenceSum);
---
+++ 1_2/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] lines = File.ReadAllLines("input");
+Span<int> list1 = stackalloc int[lines.Length];
+int list1Length = 0;
+Span<int> list2 = stackalloc int[lines.Length];
+int list2Length = 0;
+Dictionary<int, int> nCounters = new Dictionary<int, int>(lines.Length);
+
+foreach (var line in lines) {
+    string[] strings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int i = Convert.ToInt32(strings[0]);
+    int j = Convert.ToInt32(strings[1]);
+    list1[list1Length++] = i;
+    list2[list2Length++] = j;
+}
+
+foreach (int i in list2) {
+    int hasValue = 0;
+    nCounters.TryGetValue(i, out hasValue);
+    nCounters.Remove(i);
+    nCounters.Add(i, ++hasValue);
+}
+
+int sum = 0;
+
+foreach (int i in list1) {
+    sum += i * nCounters.GetValueOrDefault(i);
+}
+
+Console.WriteLine(sum);
