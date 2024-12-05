3c6fdd886a102c943bec8bed760edbbdabfa15e7
Kerem Göksu <superkerem13@gmail.com>
2024-12-05 12:48:33 +0300

    Day 4 Part 2

---
+++ 4_2/Program.cs
@@ -0,0 +1,20 @@
+﻿char[][] board = File.ReadAllLines("input").Select(x => x.ToArray()).ToArray();
+
+int count = 0;
+
+for (int i = 0; i < board.Length; i++)
+{
+    for (int j = 0; j < board.Length; j++)
+    {
+        if (board[i][j] != 'A') continue;
+        if (i < 1 || j < 1) continue;
+        if (board.Length <= i + 1 || board[i + 1].Length <= j + 1) continue;
+        if ((board[i - 1][j - 1] != 'M' || board[i + 1][j + 1] != 'S')
+         && (board[i - 1][j - 1] != 'S' || board[i + 1][j + 1] != 'M')) continue;
+        if ((board[i + 1][j - 1] != 'M' || board[i - 1][j + 1] != 'S')
+         && (board[i + 1][j - 1] != 'S' || board[i - 1][j + 1] != 'M')) continue;
+        count++;
+    }
+}
+
+Console.WriteLine(count);
